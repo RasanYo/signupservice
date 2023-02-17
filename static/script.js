f36db720e7c6b3f14ea0f23be29556fe57055258
Pascal  Schütze <pascalschutze@Pascals-MBP.fritz.box>
2023-02-17 14:39:15 +0100

    Flask app, Frontend with html, css and js

---
+++ static/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var email = document.querySelector('#email').value;
+    var password = document.querySelector('#password').value;
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function() {
+      setTimeout(function(){ window.close(); }, 10000);
+    };
+    xhr.send(JSON.stringify({'email': email, 'password': password}));
+  });
+});
+
+
+/*document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    setTimeout(function(){ window.close(); }, 10000);
+  });
+});*/
